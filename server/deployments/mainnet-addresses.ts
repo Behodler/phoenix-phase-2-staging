dc94b0c11091b4d7006d8799084f12f013934f76
Justin <justinheyns@gmail.com>
2026-03-13 23:28:36 +0200

    removed comments

--- server/deployments/mainnet-addresses.ts
+++ server/deployments/mainnet-addresses.ts
@@ -2,29 +2,6 @@
 // Chain ID: 1 (mainnet)
 import { ContractAddresses } from './addresses';
 
-// export interface ContractAddresses {
-//   PhUSD: string,
-//   USDC: string,
-//   USDT: string,
-//   Dola: string,
-//   USDS: string,
-//   Toke: string,
-//   EYE: string,
-//   Pauser: string,
-//   YieldStrategyUSDT: string,
-//   YieldStrategyDola: string,
-//   YieldStrategyUSDS: string,
-//   YieldStrategyUSDC: string,
-//   PhusdStableMinter: string,
-//   StableYieldAccumulator: string,
-//   PhlimboEA: string,
-//   AutoDOLA: string,
-//   AutoUSDC: string,
-//   MainRewarder: string,
-//   MainRewarderUSDC: string,
-//   DepositView: string,
-// }
-
 export const mainnetAddresses: ContractAddresses = {
   // Deployed Phase 2 contracts
   Pauser: "0x7c5A8EeF1d836450C019FB036453ac6eC97885a3",
