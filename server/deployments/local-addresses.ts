bdbc08b7ee7b109919498b1fc2372183fe97a35d
Justin <justinheyns@gmail.com>
2026-03-18 11:51:16 +0200

    Sepolia state fix

--- server/deployments/local-addresses.ts
+++ server/deployments/local-addresses.ts
@@ -1,4 +1,4 @@
-// Generated from local.json on 2026-03-17T16:09:18.362Z
+// Generated from local.json on 2026-03-18T08:53:39.110Z
 // Chain ID: 31337 (anvil)
 
 import { ContractAddresses } from './addresses';
