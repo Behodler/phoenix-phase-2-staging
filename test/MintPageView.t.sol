e760a19043aee6d1e1119f2273e93147a8db5e73
Justin <12531162+gititGoro@users.noreply.github.com>
2026-04-17 16:54:03 +0200

    [story-042] Update MintPageView tests for USDS rename
    - Rename susds/dispatcherSUSDS -> usds/dispatcherUSDS in setUp
    - Update existing assertions that referenced "sUSDS-*" field names and
      "sUSDS allowance/price/balance/..." labels to "USDS-*"
    - Add testGetNames_USDSFieldsNamedCorrectly: iterates indices 18-23 and
      asserts each name starts with the bytes "USDS-"
    - Add testGetData_USDSDispatcherIndex4: asserts data[23] == 4 so a future
      dispatcher reshuffle that breaks the view trips this test
    
    All 8 MintPageView tests pass locally. Other pre-existing fork/env-var
    test failures in the suite are unrelated.

--- test/MintPageView.t.sol
+++ test/MintPageView.t.sol
@@ -56,13 +56,13 @@ contract MintPageViewTest is Test {
     MockERC20 public eye;
     MockERC20 public scx;
     MockERC20 public flax;
-    MockERC20 public susds;
+    MockERC20 public usds;
     MockERC20 public wbtc;
 
     MockDispatcher public dispatcherEYE;
     MockDispatcher public dispatcherSCX;
     MockDispatcher public dispatcherFlax;
-    MockDispatcher public dispatcherSUSDS;
+    MockDispatcher public dispatcherUSDS;
     MockDispatcher public dispatcherWBTC;
 
     address public user = makeAddr("user");
@@ -75,7 +75,7 @@ contract MintPageViewTest is Test {
         eye = new MockERC20("EYE", "EYE");
         scx = new MockERC20("SCX", "SCX");
         flax = new MockERC20("Flax", "FLAX");
-        susds = new MockERC20("sUSDS", "sUSDS");
+        usds = new MockERC20("USDS", "USDS");
         wbtc = new MockERC20("WBTC", "WBTC");
 
         // Deploy NFTMinter
@@ -88,14 +88,14 @@ contract MintPageViewTest is Test {
         dispatcherEYE = new MockDispatcher(address(eye));
         dispatcherSCX = new MockDispatcher(address(scx));
         dispatcherFlax = new MockDispatcher(address(flax));
-        dispatcherSUSDS = new MockDispatcher(address(susds));
+        dispatcherUSDS = new MockDispatcher(address(usds));
         dispatcherWBTC = new MockDispatcher(address(wbtc));
 
-        // Register dispatchers in order: index 1=EYE, 2=SCX, 3=Flax, 4=sUSDS, 5=WBTC
+        // Register dispatchers in order: index 1=EYE, 2=SCX, 3=Flax, 4=USDS (BalancerPoolerV2), 5=WBTC
         nftMinter.registerDispatcher(address(dispatcherEYE), 1 ether, 100); // 1%
         nftMinter.registerDispatcher(address(dispatcherSCX), 2 ether, 200); // 2%
         nftMinter.registerDispatcher(address(dispatcherFlax), 0.5 ether, 50); // 0.5%
-        nftMinter.registerDispatcher(address(dispatcherSUSDS), 10 ether, 300); // 3%
+        nftMinter.registerDispatcher(address(dispatcherUSDS), 10 ether, 300); // 3%
         nftMinter.registerDispatcher(address(dispatcherWBTC), 0.001 ether, 500); // 5%
 
         // Deploy MintPageView
@@ -105,7 +105,7 @@ contract MintPageViewTest is Test {
             address(eye),
             address(scx),
             address(flax),
-            address(susds),
+            address(usds),
             address(wbtc)
         );
     }
@@ -142,13 +142,13 @@ contract MintPageViewTest is Test {
         assertEq(names[16], "Flax-nftBalance");
         assertEq(names[17], "Flax-dispatcherIndex");
 
-        // sUSDS fields
-        assertEq(names[18], "sUSDS-allowance");
-        assertEq(names[19], "sUSDS-price");
-        assertEq(names[20], "sUSDS-growthBasisPoints");
-        assertEq(names[21], "sUSDS-balance");
-        assertEq(names[22], "sUSDS-nftBalance");
-        assertEq(names[23], "sUSDS-dispatcherIndex");
+        // USDS fields
+        assertEq(names[18], "USDS-allowance");
+        assertEq(names[19], "USDS-price");
+        assertEq(names[20], "USDS-growthBasisPoints");
+        assertEq(names[21], "USDS-balance");
+        assertEq(names[22], "USDS-nftBalance");
+        assertEq(names[23], "USDS-dispatcherIndex");
 
         // WBTC fields
         assertEq(names[24], "WBTC-allowance");
@@ -176,42 +176,42 @@ contract MintPageViewTest is Test {
         assertEq(data[0], 0, "EYE allowance should be 0");
         assertEq(data[6], 0, "SCX allowance should be 0");
         assertEq(data[12], 0, "Flax allowance should be 0");
-        assertEq(data[18], 0, "sUSDS allowance should be 0");
+        assertEq(data[18], 0, "USDS allowance should be 0");
         assertEq(data[24], 0, "WBTC allowance should be 0");
 
         // Prices should match registered values
         assertEq(data[1], 1 ether, "EYE price");
         assertEq(data[7], 2 ether, "SCX price");
         assertEq(data[13], 0.5 ether, "Flax price");
-        assertEq(data[19], 10 ether, "sUSDS price");
+        assertEq(data[19], 10 ether, "USDS price");
         assertEq(data[25], 0.001 ether, "WBTC price");
 
         // Growth basis points should match registered values
         assertEq(data[2], 100, "EYE growthBasisPoints");
         assertEq(data[8], 200, "SCX growthBasisPoints");
         assertEq(data[14], 50, "Flax growthBasisPoints");
-        assertEq(data[20], 300, "sUSDS growthBasisPoints");
+        assertEq(data[20], 300, "USDS growthBasisPoints");
         assertEq(data[26], 500, "WBTC growthBasisPoints");
 
         // All balances should be 0
         assertEq(data[3], 0, "EYE balance should be 0");
         assertEq(data[9], 0, "SCX balance should be 0");
         assertEq(data[15], 0, "Flax balance should be 0");
-        assertEq(data[21], 0, "sUSDS balance should be 0");
+        assertEq(data[21], 0, "USDS balance should be 0");
         assertEq(data[27], 0, "WBTC balance should be 0");
 
         // All NFT balances should be 0
         assertEq(data[4], 0, "EYE nftBalance should be 0");
         assertEq(data[10], 0, "SCX nftBalance should be 0");
         assertEq(data[16], 0, "Flax nftBalance should be 0");
-        assertEq(data[22], 0, "sUSDS nftBalance should be 0");
+        assertEq(data[22], 0, "USDS nftBalance should be 0");
         assertEq(data[28], 0, "WBTC nftBalance should be 0");
 
         // Dispatcher indices
         assertEq(data[5], 1, "EYE dispatcherIndex should be 1");
         assertEq(data[11], 2, "SCX dispatcherIndex should be 2");
         assertEq(data[17], 3, "Flax dispatcherIndex should be 3");
-        assertEq(data[23], 4, "sUSDS dispatcherIndex should be 4");
+        assertEq(data[23], 4, "USDS dispatcherIndex should be 4 (BalancerPoolerV2)");
         assertEq(data[29], 5, "WBTC dispatcherIndex should be 5");
 
         // All burn totals should be 0
@@ -225,7 +225,7 @@ contract MintPageViewTest is Test {
         eye.mint(user, 100 ether);
         scx.mint(user, 50 ether);
         flax.mint(user, 200 ether);
-        susds.mint(user, 1000 ether);
+        usds.mint(user, 1000 ether);
         wbtc.mint(user, 5 ether);
 
         // Set allowances
@@ -233,7 +233,7 @@ contract MintPageViewTest is Test {
         eye.approve(address(nftMinter), 10 ether);
         scx.approve(address(nftMinter), 20 ether);
         flax.approve(address(nftMinter), 30 ether);
-        susds.approve(address(nftMinter), 40 ether);
+        usds.approve(address(nftMinter), 40 ether);
         wbtc.approve(address(nftMinter), 0.5 ether);
         vm.stopPrank();
 
@@ -243,22 +243,39 @@ contract MintPageViewTest is Test {
         assertEq(data[0], 10 ether, "EYE allowance");
         assertEq(data[6], 20 ether, "SCX allowance");
         assertEq(data[12], 30 ether, "Flax allowance");
-        assertEq(data[18], 40 ether, "sUSDS allowance");
+        assertEq(data[18], 40 ether, "USDS allowance");
         assertEq(data[24], 0.5 ether, "WBTC allowance");
 
         // Balances
         assertEq(data[3], 100 ether, "EYE balance");
         assertEq(data[9], 50 ether, "SCX balance");
         assertEq(data[15], 200 ether, "Flax balance");
-        assertEq(data[21], 1000 ether, "sUSDS balance");
+        assertEq(data[21], 1000 ether, "USDS balance");
         assertEq(data[27], 5 ether, "WBTC balance");
 
         // Dispatcher indices
         assertEq(data[5], 1, "EYE dispatcherIndex");
         assertEq(data[11], 2, "SCX dispatcherIndex");
         assertEq(data[17], 3, "Flax dispatcherIndex");
-        assertEq(data[23], 4, "sUSDS dispatcherIndex");
+        assertEq(data[23], 4, "USDS dispatcherIndex");
         assertEq(data[29], 5, "WBTC dispatcherIndex");
+    }
+
+    function testGetNames_USDSFieldsNamedCorrectly() public view {
+        string[] memory names = view_.getNames();
+        bytes memory prefix = bytes("USDS-");
+        for (uint256 i = 18; i <= 23; i++) {
+            bytes memory nameBytes = bytes(names[i]);
+            assertGe(nameBytes.length, prefix.length, "name too short");
+            for (uint256 j = 0; j < prefix.length; j++) {
+                assertEq(nameBytes[j], prefix[j], "name does not start with USDS-");
+            }
+        }
+    }
+
+    function testGetData_USDSDispatcherIndex4() public view {
+        uint256[] memory data = view_.getData(user);
+        assertEq(data[23], 4, "USDS dispatcher index must be 4 (BalancerPoolerV2)");
     }
 
     function testGetNamesAndGetDataLengthsMatch() public view {
